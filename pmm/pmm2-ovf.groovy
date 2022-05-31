9ae85e0846c4502fb589db130b92ce62497e936b
Nikita Beletskii <nikita.beletskii@percona.com>
2022-05-31 11:33:54 +0000

    fix

--- pmm/pmm2-ovf.groovy
+++ pmm/pmm2-ovf.groovy
@@ -72,7 +72,7 @@ pipeline {
                         -var 'pmm_client_repos=original experimental' \
                         -var 'pmm_client_repo_name=percona-experimental-x86_64' \
                         -var 'pmm2_server_repo=experimental' \
-                        -only virtualbox-ovf -color=false packer/pmm2.json \
+                        -only -only pmm2.virtualbox-ovf.image -color=false packer/pmm2.pkr.hcl \
                             | tee build.log
                     """
                 }
